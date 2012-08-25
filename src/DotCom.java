b17aca9385106e3acb812171d87a79a05da21b23
Tony Nguyen <tony@silentguy.com>
2012-08-25 14:03:52 -0500

    Code cleanup. Added GetName

--- src/DotCom.java
+++ src/DotCom.java
@@ -1,29 +1,32 @@
-import java.util.ArrayList;
+import java.util.*;
+
 public class DotCom {
-	String name;
-	
-	public void setName(String n){
-		name = n;
-	}
-	
+
 	private ArrayList<String> locationCells;
-	// private int numOfHits;
-	// don't need that now
+	private String name;
 	
 	public void setLocationsCells(ArrayList<String> loc) {
 		locationCells = loc;
 	}
-	
+	public void setName(String n){
+		name = n;
+		System.out.println("Created DotCom: " + name);
+	}
+	public String getName(){
+		return name;
+	}
 	String checkYourself(String userInput) {
 		String result = "miss";
 		int index = locationCells.indexOf(userInput);
 		if (index >= 0){
 			locationCells.remove(index);
-			
+			System.out.println("Good hit.");
 			if (locationCells.isEmpty()) {
 				result = "kill";
+				System.out.println("Damn. you killed " + name);
 			} else {
 				result = "hit";
+				System.out.println("You hit " + name);
 			}
 		}
 		
